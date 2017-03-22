1edce0874a230a95fe4fc1eacf61a076a9da6aa0
qqzhw <285665394@qq.com>
2017-03-22 16:30:13 +0800

    chga

---
+++ Pvirtech.CommandSystem.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Pvirtech.CommandSystem.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Pvirtech.CommandSystem.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1e777765-cc92-46a6-a445-a1402db7879c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Pvirtech.CommandSystem.Test/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Windows;
+using Pvirtech.Metro.Controls;
+
+namespace Pvirtech.CommandSystem.Test
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			MetroWindow d = new  MetroWindow();
+			d.Show();
+			 
+		}
+	}
+}
