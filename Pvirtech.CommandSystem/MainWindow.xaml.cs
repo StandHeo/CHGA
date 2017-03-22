dc24774acd6a9d864bca2eb1a4d796f02c7efd8c
qqzhw <285665394@qq.com>
2017-03-22 18:14:20 +0800

    主窗口

---
+++ Pvirtech.CommandSystem/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pvirtech.CommandSystem
+{
+	/// <summary>
+	/// MainWindow.xaml 的交互逻辑
+	/// </summary>
+	public partial class MainWindow 
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void MetroWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+		{
+
+		}
+
+		private void LaunchMahAppsOnGitHub(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void LauchCleanDemo(object sender, RoutedEventArgs e)
+		{
+
+		}
+	}
+}
